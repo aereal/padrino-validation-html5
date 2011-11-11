77e297add7bec6dd12264c90d5eca4bd6000df06
AOKI Hanae <aereal@kerare.org>
2011-11-12 07:47:45 +0900

    remove static values from spec

--- spec/padrino/validation/html5_spec.rb
+++ spec/padrino/validation/html5_spec.rb
@@ -2,53 +2,66 @@
 
 describe Padrino::Validation::HTML5 do
 	before do
+		@request_path = '/register'
 		Padrino::Helpers::FormBuilder::StandardFormBuilder.send :include, Padrino::Validation::HTML5
-		class User < ActiveModel::Base
+	end
+
+	let(:model) {
+		User ||= Class.new(ActiveModel::Base) do
 			@attrs = [:name, :age]
 			define_attribute_methods @attrs
 			attr_accessor *@attrs
 		end
-	end
+	}
+	let!(:model_instance) { model.new }
+	let(:template_builder) { builder_instance(model_instance) }
+	let(:field_id) { template_builder.send(:field_id, attribute) }
 
 	context "when model has field which should be present" do
 		before do
-			User.validates_presence_of :name
+			model.validates_presence_of attribute
 		end
 
+		let(:attribute) { :name }
+
 		subject do
-			form_for(User.new, '/register') {|f| f.text_field :name }
+			form_for(model_instance, @request_path) {|f| f.text_field attribute }
 		end
 
 		it "injects `required' attribute to `input' element" do
-			should have_tag('input', required: 'required')
+			should have_tag('input', id: field_id, required: 'required')
 		end
 	end
 
 	context "when model has field which restricted max length" do
 		before do
-			User.validates_length_of :name, maximum: 255
+			model.validates_length_of attribute, maximum: 255
 		end
 
+		let(:attribute) { :name }
+
 		subject do
-			form_for(User.new, '/register') {|f| f.text_field :name }
+			form_for(model_instance, @request_path) {|f| f.text_field attribute }
 		end
 
 		it "injects `maxlength' attribute to `input' element" do
-			should have_tag('input', maxlength: '255')
+			should have_tag('input', id: field_id, maxlength: '255')
 		end
 	end
 
 	context "when model has field which is inclusive of any range" do
 		before do
-			User.validates_inclusion_of :age, in: 20..30
+			model.validates_inclusion_of attribute, in: 20..30
 		end
 
+		let(:attribute) { :age }
+
 		subject do
-			form_for(User.new, '/register') {|f| f.text_field :age }
+			form_for(model_instance, @request_path) {|f| f.text_field attribute }
 		end
 
 		it "injects `max' attribute to element" do
-			should have_tag('input', max: '30', min: '20')
+			should have_tag('input', id: field_id, max: '30', min: '20')
 		end
 	end
 end
